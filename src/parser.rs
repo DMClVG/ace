fcdfecb260b58da9eaada9190c87cf4cfb2a4cb2
a <48882585+DMClVG@users.noreply.github.com>
2022-07-17 17:33:48 +0200

    Fix double scope x2

--- src/parser.rs
+++ src/parser.rs
@@ -105,7 +105,7 @@ impl<'a: 'b, 'b> Parser<'a, 'b> {
             },
             Fun => {
                 if self.match_adv(&Colon) {
-                    Ok(Statement::FunDecl(self.params().map_err(|e| vec![e])?, Box::new(self.block(true)?)))
+                    Ok(Statement::FunDecl(self.params().map_err(|e| vec![e])?, Box::new(self.block(false)?)))
                 } else {
                     Ok(Statement::FunDecl(vec![], Box::new(self.block(false)?)))
                 }
