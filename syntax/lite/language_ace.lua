9673122617eab63b17e8624d4cf1ea9278e8e2f7
a <48882585+DMClVG@users.noreply.github.com>
2022-07-28 12:45:34 +0200

    Lite-xl syntax highlighting

---
+++ syntax/lite/language_ace.lua
@@ -0,0 +1,33 @@
+-- mod-version:2 -- lite-xl 2.0
+
+local syntax = require "core.syntax"
+
+syntax.add {
+    files = "%.ace",
+    comment = "#",
+    patterns = {
+        { pattern = { '"', '"', '\\' }, type = "string" },
+        { pattern = "#.-\n", type = "comment" },
+        { pattern = "-?%d+[%d%.eE]*", type = "number" },
+        { pattern = "-?%.?%d+", type = "number" },
+        { pattern = "[<>!=]=", type = "operator" },
+        { pattern = "[%+%*-%%/]", type = "operator" },
+        { pattern = "[%a_][%w_]*%s*%f[(]", type = "function" },
+        { pattern = "[%a_][%w_]*", type = "symbol" }
+    },
+    symbols = {
+        ["if"] = "keyword",
+        ["fun"] = "keyword",
+        ["while"] = "keyword",
+        ["for"] = "keyword",
+        ["else"] = "keyword",
+        ["return"] = "keyword",
+        ["and"] = "keyword",
+        ["or"] = "keyword",
+        ["loop"] = "keyword",
+
+        ["nil"] = "literal",
+        ["true"] = "literal",
+        ["false"] = "literal",
+    }
+}
